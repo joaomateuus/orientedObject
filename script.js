1330762520d402dc4fc1becb68bcff8248f94c9e
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-02-15 17:01:17 -0300

    comminting frontend

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 class Produto {
-    constructor(){
-
+    /*we´ve to use this, to reference our variables,
+    turning it able to have acess in the others instances*/
+    //and we use the constructor method, like a model to get
+    //our atributtes
+    constructor() {
+        this.id = 0;
+        this.productName = ''
     };
 
     adicionar(){
         alert('Testando nossa função js');
     };
+    excluir(){
+        //--
+        alert('O item foi excluido!!');
+    }
 };
 
 const produto = new Produto();
