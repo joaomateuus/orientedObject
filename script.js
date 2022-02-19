509de90d0250c1b159732da7579b9eb59f03e5eb
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-02-18 21:45:06 -0300

    adding and clean form field

--- script.js
+++ script.js
@@ -10,30 +10,8 @@ class Product {
         this.arrayProducts = [];
     };
 
-    //it will save in the array
-    save() {
-        //here I´ve passed the value that was stored in our input reader function
-        let product = this.inputReader();
-        //calling our validator to not allow input erros
-        this.fieldValidator(product);
-        //if it return true we will save
-        //if wont, will be displayed the message as set in the function
-        if(this.fieldValidator(product)){
-            this.add(product);
-            alert('Your product has been saved')
-        }
-    
-        console.log(this.arrayProducts);
-    };
-    
-    // method that will store the object in the array
-    add(product) {
-        this.arrayProducts.push(product);
-        this.id++;
-    }
-    
-    //this func will get the value in inputFields
-    inputReader() {
+      //this func will get the value in inputFields
+      inputReader() {
         //object that will store the product value 
         let product = {};
 
@@ -72,12 +50,61 @@ class Product {
         return true;
     };
 
+    //it will save in the array
+    save() {
+        //here I´ve passed the value that was stored in our input reader function
+        let product = this.inputReader();
+        //calling our validator to not allow input erros
+        this.fieldValidator(product);
+        //if it return true we will save in the object
+        //if wont, will be displayed the message as set in the function
+        if(this.fieldValidator(product)){
+            this.add(product);
+            alert('Your product has been saved')
+        }
+        this.readTable();
+        this.clean();
+    };
+    
+    // method that will store the object in the array
+    add(product) {
+        this.arrayProducts.push(product);
+        this.id++;
+    }
+    
+    //func that will run over our, catching values
+    readTable() {
+        let tbody = document.getElementById('tbody');
 
-    // it will clear the fields
-    clean(){
-        //--
-       
+        for ( let i = 0; i < this.arrayProducts.length; i++ ) {
+            //func to create new row as products are coming
+            let tr = tbody.insertRow();
+
+            //passing the insert func for tds, columns 
+            let td_id = tr.insertCell();
+            let td_name = tr.insertCell();
+            let td_value = tr.insertCell();
+            let td_category = tr.insertCell();
+            
+            //catching the array data
+            td_id.innerText = this.arrayProducts[i].id;
+            td_name.innerText = this.arrayProducts[i].name;
+            td_value.innerText = this.arrayProducts[i].value;
+            td_category.innerText = this.arrayProducts[i].category; 
+        };
+    } ;
+
+    
+
+    // it will clear the form fields
+    clean() {
+       document.getElementById('name').value = '';
+       document.getElementById('value').value = '';
+       document.getElementById('category').value = '';
     }
 };
+
+
+
 
 const product = new Product();
